b8e7ebff3b2c9e33c32a4684f70ab0b4470dcb7a
OttoCarius <ottocarius1993@gmail.com>
2022-08-07 15:17:30 +0300

    1 task - done;

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const { startBtn, stopBtn } = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', '');
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
